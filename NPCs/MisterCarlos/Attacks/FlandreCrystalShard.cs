601165c3fe2b54f5a2a90fcdb2635067f4d8a38c
ElCholoGamer <josedanielgrayson@gmail.com>
2021-09-13 13:24:34 -0500

    init crystal shard attack

---
+++ NPCs/MisterCarlos/Attacks/FlandreCrystalShard.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+
+namespace MisterCarlosMod.NPCs.MisterCarlos.Attacks
+{
+    public class FlandreCrystalShard : NPCAttack<MisterCarlos>
+    {
+        public override float Duration => 600f;
+
+        public FlandreCrystalShard(MisterCarlos carlos) : base(carlos)
+        {
+
+        }
+
+        public override void AI()
+        {
+            
+        }
+    }
+}
