2e38e1e89766f8b2436839bd7f5e8a5365b00633
ElCholoGamer <josedanielgrayson@gmail.com>
2021-09-13 16:28:33 -0500

    add rotating crystal vile shard

---
+++ Projectiles/CrystalShard.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MisterCarlosMod.Projectiles
+{
+    public class CrystalShard : ModProjectile
+    {
+        public override string Texture => "Terraria/Projectile_" + ProjectileID.CrystalVileShardShaft;
+
+        private bool continueShard = true;
+
+        private const int LifeSpan = 60;
+
+        public int ShardsLeft
+        {
+            get => (int)projectile.ai[0];
+        }
+
+        public float ContinueTimer
+        {
+            get => projectile.ai[1];
+            set => projectile.ai[1] = value;
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.hostile = true;
+            projectile.penetrate = -1;
+            projectile.width = projectile.height = 32;
+            projectile.timeLeft = LifeSpan;
+        }
+
+        public override void AI()
+        {
+            projectile.velocity *= 0f;
+            int alphaSpeed = (int)Math.Ceiling(255f / LifeSpan);
+            projectile.alpha = Math.Min(projectile.alpha + alphaSpeed, 255);
+
+            if (ShardsLeft <= 0)
+                continueShard = false;
+
+            if (Main.netMode != NetmodeID.MultiplayerClient && continueShard && ContinueTimer++ > 0)
+            {
+                Vector2 addPosition = projectile.rotation.ToRotationVector2() * projectile.height;
+
+                int projID = Projectile.NewProjectile(
+                    projectile.Center - addPosition,
+                    Vector2.Zero,
+                    projectile.type,
+                    projectile.damage,
+                    0f,
+                    projectile.owner,
+                    ShardsLeft - 1);
+
+                Main.projectile[projID].rotation = projectile.rotation;
+
+                continueShard = false;
+            }
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+            Texture2D texture = Main.projectileTexture[ShardsLeft > 0 ? ProjectileID.CrystalVileShardShaft : ProjectileID.CrystalVileShardHead];
+
+            Vector2 origin = new Vector2(texture.Width / 2f, texture.Height / 2f);
+
+            spriteBatch.Draw(
+                texture,
+                projectile.position - Main.screenPosition,
+                null,
+                projectile.GetAlpha(lightColor),
+                projectile.rotation - MathHelper.PiOver2,
+                origin,
+                projectile.scale,
+                SpriteEffects.None,
+                0f);
+
+            return false;
+        }
+
+        public override bool CanHitPlayer(Player target)
+        {
+            return projectile.alpha < 120;
+        }
+    }
+}
---
+++ Projectiles/FlandreCrystalShard/RotatingCrystalVileShard.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MisterCarlosMod.Projectiles.FlandreCrystalShard
+{
+    public class RotatingCrystalVileShard : ModProjectile
+    {
+        public override string Texture => "Terraria/Item_" + ItemID.CrystalVileShard;
+
+        public float RotationSpeed
+        {
+            get => projectile.ai[0];
+            set => projectile.ai[0] = value;
+        }
+
+        public float AttackTimer
+        {
+            get => projectile.ai[1];
+            set => projectile.ai[1] = value;
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.hostile = true;
+            projectile.penetrate = -1;
+            projectile.width = projectile.height = 32;
+            projectile.tileCollide = false;
+            projectile.ignoreWater = true;
+        }
+
+        public override void AI()
+        {
+            projectile.rotation += RotationSpeed;
+
+            if (Main.netMode != NetmodeID.MultiplayerClient && AttackTimer++ > 5f)
+            {
+                int crystals = 4;
+                float separation = MathHelper.TwoPi / crystals;
+
+                for (int i = 0; i < crystals; i++)
+                {
+                    int projID = Projectile.NewProjectile(
+                        projectile.Center,
+                        Vector2.Zero,
+                        ModContent.ProjectileType<CrystalShard>(),
+                        projectile.damage,
+                        0f,
+                        Main.myPlayer,
+                        15);
+
+                    Main.projectile[projID].rotation = projectile.rotation - MathHelper.PiOver4 + i * separation;
+                }
+
+                Main.PlaySound(SoundID.Item101, projectile.Center);
+
+                AttackTimer = 0f;
+            }
+        }
+    }
+}
